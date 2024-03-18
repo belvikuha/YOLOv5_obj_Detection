bb8466a1907a875cf2eef83202a4308d7c273406
belvikuha <belviktory0.0@gmail.com>
2024-03-18 22:04:42 +0200

    add RabbitMQ connection

--- main.py
+++ main.py
@@ -1,32 +1,58 @@
 import torch
 import cv2
 import numpy as np
+import pika
+import io
+
+from websocket import create_connection
 
 # Загрузка модели
 model = torch.hub.load('ultralytics/yolov5', 'yolov5l', pretrained=True)
 
 # Функция для распознавания и отображения объектов
-def detect_objects(image_path):
-    # Загрузка изображения
-    img = cv2.imread(image_path)
+def detect_objects(image_bytes):
+    # Преобразование буфера в изображение
+    image_np = np.frombuffer(image_bytes, dtype=np.uint8)
+    img = cv2.imdecode(image_np, flags=cv2.IMREAD_COLOR)
 
     # Проверка, что изображение успешно загружено
     if img is None:
-        print(f"Не удалось загрузить изображение: {image_path}")
+        print("Не удалось загрузить изображение")
         return
 
     # Обнаружение объектов
     results = model(img)
 
-    for i, (x, y, w, h, conf, cls) in enumerate(results.xywh[0]):
-        label = results.names[int(cls)]
-        print(f"Объект {i}: {label} (Уверенность: {conf:.2f}), Координаты: x={x:.0f}, y={y:.0f}, w={w:.0f}, h={h:.0f}")
-
     # Визуализация результатов
-    cv2.imshow('YOLOv5 Detection', np.squeeze(results.render()))
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
+    rendered_img = np.squeeze(results.render())
 
-# Пример использования
-image_path = 'image.jpg'  # Укажите путь к вашему изображению
-detect_objects(image_path)
+    # Кодирование изображения в формат JPEG для отправки по веб-сокету
+    _, img_encoded = cv2.imencode('.jpg', rendered_img)
+    return img_encoded.tobytes()
+
+# Функция обработки сообщений из очереди
+def callback(ch, method, properties, body):
+    print("Получено изображение из очереди")
+    processed_image = detect_objects(body)
+
+    # Подключение к WebSocket и отправка обработанного изображения
+    try:
+        ws = create_connection("ws://localhost:8099")
+        ws.send_binary(processed_image)
+        ws.close()
+    except Exception as e:
+        print("Ошибка WebSocket:", e)
+
+# Подключение к RabbitMQ
+connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
+channel = connection.channel()
+
+# Создание очереди
+channel.queue_declare(queue='imagesQueue')
+
+# Установка обработчика сообщений
+channel.basic_consume(queue='imagesQueue', on_message_callback=callback, auto_ack=True)
+
+print('Ожидание изображений...')
+channel.start_consuming()
+
